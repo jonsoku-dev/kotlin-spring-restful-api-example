dc0eb214e47cf488c90f62c77d86f95539906704
jonsoku2 <whdtjr2792@gmail.com>
2021-01-05 00:37:27 +0900

    5. domain entity화

--- src/main/kotlin/com/tamastudy/kotlinrestapi/domain/Person.kt
+++ src/main/kotlin/com/tamastudy/kotlinrestapi/domain/Person.kt
@@ -1,7 +1,21 @@
 package com.tamastudy.kotlinrestapi.domain
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.SequenceGenerator
+
+@Entity
 data class Person(
+        @Id
+        @SequenceGenerator(name = PERSON_SEQUENCE, sequenceName = PERSON_SEQUENCE, initialValue = 1, allocationSize = 1)
         val id: Long? = 1L,
+//        @Column(name = "name")
         var name: String = "",
+//        @Column(name = "last_name")
         var lastName: String? = null
-)
+) {
+    companion object {
+        const val PERSON_SEQUENCE: String = "PERSON_SEQUENCE"
+    }
+}
