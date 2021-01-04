63f02ebcd9ec0c9d0e1996a520e40707db22d7c7
jonsoku2 <whdtjr2792@gmail.com>
2021-01-05 00:53:42 +0900

    7. db 연결 (application.yml)

--- src/main/kotlin/com/tamastudy/kotlinrestapi/domain/Person.kt
+++ src/main/kotlin/com/tamastudy/kotlinrestapi/domain/Person.kt
@@ -1,14 +1,12 @@
 package com.tamastudy.kotlinrestapi.domain
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.SequenceGenerator
+import javax.persistence.*
 
 @Entity
 data class Person(
         @Id
         @SequenceGenerator(name = PERSON_SEQUENCE, sequenceName = PERSON_SEQUENCE, initialValue = 1, allocationSize = 1)
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = PERSON_SEQUENCE)
         val id: Long? = 1L,
 //        @Column(name = "name")
         var name: String = "",
